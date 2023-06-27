35abe4dba6cc8dcc1d1a238d5d1bb6315a3b839a
omkarupa <omkarmh009@gmail.com>
2023-06-27 22:36:11 +0800

    Eureka Server Commmit

---
+++ eureka-server/src/test/java/com/ou_solutions/microservices/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ou_solutions.microservices.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
